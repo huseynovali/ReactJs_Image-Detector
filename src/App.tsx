ebb9acd4e9d171c798bfb2b61d27627ae53541a3
User <huseynovali612@gmail.com>
2024-05-08 00:17:23 +0400

    complated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import * as nsfwjs from 'nsfwjs'
+import * as nsfwjs from "nsfwjs";
 function App() {
+  const [status, setStatus] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
 
@@ -8,19 +10,18 @@ function App() {
       console.error("No file selected.");
       return;
     }
-    const model = await nsfwjs.load('/model/',{ size: 299 })
+    const model = await nsfwjs.load("/model/", { size: 299 });
     const reader = new FileReader();
+    setLoading(true);
     reader.onload = async (event) => {
-    
       const img = new Image();
-      img.onload = async () => {
-       
-        const predictions = await model.classify(img)
 
-        console.log("Predictions: ");
-        console.log(predictions);
+      img.onload = async () => {
+        const predictions = await model.classify(img);
+        setStatus(predictions[0].className === "Neutral");
+        setLoading(false);
       };
-      img.src = event.target?.result as string; 
+      img.src = event.target?.result as string;
     };
     reader.readAsDataURL(file);
   };
@@ -31,6 +32,14 @@ function App() {
       <p>Upload a photo and get the description of the photo</p>
       <div>
         <input type="file" onChange={handleUpload} />
+        {}
+        {loading ? (
+          <p>Loading...</p>
+        ) : status ? (
+          <p style={{ color: "green" }}>Received</p>
+        ) : (
+          <p style={{ color: "red" }}>Not Neutral</p>
+        )}
       </div>
     </div>
   );
